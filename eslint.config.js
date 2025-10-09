f83b0f8eb714cc5f5133682c0317db8e425b89f3
tisho91 <tihomir.j.marinov@gmail.com>
2025-10-09 14:33:08 +0300

    Fix lint file

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,11 @@ import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import prettierPlugin from 'eslint-plugin-prettier';
 
-// ⚡ ESLint flat config работещ с TS + Prettier
 export default [
   {
-    // Файлове за линт
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
 
-    // Parser и plugin-ите
     languageOptions: {
       parser: tsParser,
       parserOptions: {
@@ -23,18 +20,14 @@ export default [
       prettier: prettierPlugin,
     },
 
-    // Правила
     rules: {
-      // TypeScript recommended rules (имплементирани ръчно)
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
 
-      // Prettier
       'prettier/prettier': ['error', { bracketSpacing: true, singleQuote: true }],
 
-      // JS общи правила
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
       'no-unused-vars': 'off',
